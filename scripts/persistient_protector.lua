ccc6fcb7d69305faaf6702d915207adb9bb294f3
xbxbxb123 <benjaminrgoodwin@gmail.com>
2022-01-20 21:51:19 -0800

    Mostly working now

--- scripts/persistient_protector.lua
+++ scripts/persistient_protector.lua
@@ -2,9 +2,31 @@ function init()
   a = config.getParameter("effects",{})
   a = type(a) == "string" and {a} or a
   b = config.getParameter("itemName")
+  useTime = config.getParameter("useTime",0.15)
+  activeItem.setArmAngle(-math.pi / 2)
 end
 
-function activate()
-  status.addPersistentEffects(b,a)
-  item.consume(1)
+function update(dt,fireMode)
+  if not useTimer and fireMode == "primary" and player then
+    useTimer = useTime
+  end
+
+  if useTimer then
+    useTimer = math.max(0, useTimer - dt)
+
+    activeItem.setArmAngle((-math.pi / 2) * (useTimer / useTime))
+
+    if useTimer == 0 then
+--      activeItem.setArmAngle(0)
+      animator.playSound("stimuse")
+      status.addPersistentEffects(b,a)
+      activeItem.setArmAngle(-math.pi / 2)
+      item.consume(1)
+    end
+  end
+end
+
+function updateAim()
+  _, aimDirection = activeItem.aimAngleAndDirection(0, activeItem.ownerAimPosition())
+  activeItem.setFacingDirection(aimDirection)
 end
